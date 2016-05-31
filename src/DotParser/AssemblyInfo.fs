d63f3ffc45fcc850f575fd17840381f2c24e2e56
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2016-05-31 03:38:54 +0300

    Initial commit, use ProjectScaffold

---
+++ src/DotParser/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("DotParser")>]
+[<assembly: AssemblyProductAttribute("DotParser")>]
+[<assembly: AssemblyDescriptionAttribute("Project has no summmary; update build.fsx")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
+    let [<Literal>] InformationalVersion = "1.0"
