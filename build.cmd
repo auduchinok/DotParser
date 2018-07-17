959d0e2517909ecc799f7bd3fdfb8733e553f6dd
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-07-17 18:52:01 +0300

    Added packaging

--- build.cmd
+++ build.cmd
@@ -3,3 +3,4 @@ cls
 
 dotnet build DotParser.sln /P:Configuration=Release
 dotnet test tests\DotParser.Tests\DotParser.Tests.fsproj /P:Configuration=Release
+dotnet pack src\DotParser\DotParser.fsproj /P:Configuration=Release
--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 dotnet build DotParser.sln -P:Configuration=Release
 dotnet test tests/DotParser.Tests/DotParser.Tests.fsproj -P:Configuration=Release
-
+dotnet pack src/DotParser/DotParser.fsproj /P:Configuration=Release
