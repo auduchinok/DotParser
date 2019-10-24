39132af144d48ecc30c30b27c8eb0562aac405fd
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-10-24 15:44:31 +0300

    Cleanup, change namespaces/modules

--- src/DotParser/Library.fs
+++ src/DotParser/Library.fs
@@ -1,30 +1,27 @@
 // Eugene Auduchinok, 2016
 
-module DotParser
+[<RequireQualifiedAccess>]
+module DotParser.DotParser
 
 open Yard.Generators.RNGLR.Parser
 open Yard.Generators.Common.AST
-open Parser
-open Lexer
-open GraphData
-open System.Collections.Generic
+open DotParser.Parser
 open Microsoft.FSharp.Text
 
 let parse str =
-    let translateArgs = {
-            tokenToRange = fun _ -> Unchecked.defaultof<_>, Unchecked.defaultof<_>
-            zeroPosition = Unchecked.defaultof<_>
-            clearAST = false
-            filterEpsilons = false
-        }
+    let translateArgs =
+        { tokenToRange = fun _ -> Unchecked.defaultof<_>, Unchecked.defaultof<_>
+          zeroPosition = Unchecked.defaultof<_>
+          clearAST = false
+          filterEpsilons = false }
 
-    let lexbuf = Lexing.LexBuffer<_>.FromString str
+    let lexbuf = Lexing.LexBuffer<_>.FromString(str)
     let tokens = seq { while not lexbuf.IsPastEndOfStream do yield tokenize lexbuf }
 
-    let parsedGraphDataList : GraphData list =
+    let parsedGraphDataList: GraphData list =
         match buildAst tokens with
-        | Error (pos, token, msg, _, _) -> failwithf "DotParser: error on position %d, token %A: %s" pos token msg
-        | Success (ast, _, errors) -> translate translateArgs ast errors
+        | Error(pos, token, msg, _, _) -> failwithf "DotParser: error on position %d, token %A: %s" pos token msg
+        | Success(ast, _, errors) -> translate translateArgs ast errors
 
     match parsedGraphDataList with
     | data :: _ -> data
