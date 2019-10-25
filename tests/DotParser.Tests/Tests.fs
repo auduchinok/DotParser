685518a67b40230d8891432ce0103572dcf88422
Semyon <rsdpisuy@gmail.com>
2019-10-25 23:19:42 +0300

    Update tests/DotParser.Tests/Tests.fs
    Co-Authored-By: Eugene Auduchinok <eugene.auduchinok@gmail.com>

--- tests/DotParser.Tests/Tests.fs
+++ tests/DotParser.Tests/Tests.fs
@@ -11,6 +11,7 @@ let nodesCount (g: GraphData) = g.Nodes.Count
 let edgesCount (g: GraphData) = Map.fold (fun acc _ x -> acc + List.length x) 0 g.Edges
 let subgraphsCount (g: GraphData) = g.Subgraphs.Length
 
+
 let rec totalSubgraphsCount (g: GraphData) =
     g.Subgraphs.Length
     + (g.Subgraphs |> List.fold (fun cnt s -> totalSubgraphsCount s + cnt) 0) 
